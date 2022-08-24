c54c354a0fd0e8792e5577e8d1196769b2988eb6
cheng_tvfsm5a <kaikei@hotmail.com>
2022-08-24 12:29:44 +0800

    created navigation menu for home

--- webapp/dictionary.py
+++ webapp/dictionary.py
@@ -1,21 +1,30 @@
 import justpy as jp
-
+import definition
 
 class Dictionary:
     path = "/dictionary"
 
-    def serve(self):
+    @classmethod
+    def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
         div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
-        jp.Div(a=div, text="This is the Dictionary page.", classes="text-4xl m-2")
-        jp.Div(a=div, text="""
-        In literary theory, a text is any object that can be "read",
-        whether this object is a work of literature, a street sign,
-        an arrangement of buildings on a city block, or styles of clothing.
-        It is a coherent set of signs that transmits some kind of informative
-        message.
-        """, classes="text-lg")
+        jp.Div(a=div, text="Instant English Dictionary", classes="text-4xl m-2")
+        jp.Div(a=div, text="Get the definition of any English word instantly as you type", classes="text-lg")
+        input_div = jp.Div(a=div, classes="grid grid-cols-2")
+        output_div = jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
+        input_box = jp.Input(a=input_div, placeholder="Type in a word here...", outputdiv=output_div,
+                             classes="m-2 bg-gray-100 border-2 border-gray-200 "
+                                     "rounded w-64 focus:bg-white focus:outline-none "
+                                     "focus:border-purple-500 py-2, px-4")
+        input_box.on('input', cls.get_definition)
+        print(cls, req)
+
         return wp
+
+    @staticmethod
+    def get_definition(widget, msg):
+        defined = definition.Definition(widget.value).get()
+        widget.outputdiv.text = " ".join(defined)
 
 
 jp.Route(Dictionary.path, Dictionary.serve)
--- webapp/home.py
+++ webapp/home.py
@@ -4,9 +4,29 @@ import justpy as jp
 class Home:
     path = "/"
 
-    def serve(self):
+    @classmethod
+    def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
-        div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+
+        layout = jp.QLayout(a=wp, view="hHh lpR fFf")
+        header = jp.QHeader(a=layout)
+        toolbar = jp.QToolbar(a=header)
+        drawer = jp.QDrawer(a=layout, show_if_about=True, v_model="left", side="left",
+                            bordered=True)
+        scroller = jp.QScrollArea(a=drawer, classes="fit")
+        qlist = jp.QList(a=scroller)
+        a_classes = "p-2, m-2, text-lg, text-blue-400 hover:text-blue-700"
+        jp.A(a=qlist, text="Home", href="/", classes=a_classes)
+        jp.Br(a=qlist)
+        jp.A(a=qlist, text="Dictionary", href="/dictionary", classes=a_classes)
+        jp.Br(a=qlist)
+        jp.A(a=qlist, text="About", href="/about_page", classes=a_classes)
+        jp.Br(a=qlist)
+        jp.QBtn(a=toolbar, dense=True, flat=True, round=True, icon="menu",
+                click=cls.move_drawer, drawer=drawer)
+        jp.QToolbarTitle(a=toolbar, text="Instant Dictionary")
+        container = jp.QPageContainer(a=layout)
+        div = jp.Div(a=container, classes="bg-gray-200 h-screen")
         jp.Div(a=div, text="This is the Home page.", classes="text-4xl m-2")
         jp.Div(a=div, text="""
         In literary theory, a text is any object that can be "read",
@@ -16,6 +36,13 @@ class Home:
         message.
         """, classes="text-lg")
         return wp
+
+    @staticmethod
+    def move_drawer(widget, msg):
+        if widget.drawer.value:
+            widget.drawer.value = False
+        else:
+            widget.drawer.value = True
 
 
 jp.Route(Home.path, Home.serve)
